c91b9a03113a9dbce59151477fd71a664b59bb24
UTSAV0601 <66869728+UTSAV0601@users.noreply.github.com>
2025-07-07 22:33:12 -0700

    Main.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+import { Switch, Route } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/material';
